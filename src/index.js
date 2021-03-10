d41faa948380c5e4f2089dc99f88dd84a6b53480
Your Name <andrey.mglinec@gmail.com>
2021-03-10 20:36:46 +0300

    init:added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n<0){
+        n*=-1;
+        n = Number (n.toString().split('').reverse().join(''));
+        return n;
+        }else{
+        n = Number (n.toString().split('').reverse().join(''));
+        return n;
+        }
 }
